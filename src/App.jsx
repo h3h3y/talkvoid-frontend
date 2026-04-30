601c30e5ed75398205258a38fba86dcdd4a1ef79
Wannn <108000407+h3h3y@users.noreply.github.com>
2026-04-30 19:33:10 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ import {
 } from 'lucide-react';
 
 // const API_BASE_URL = 'http://localhost:8000/api';
-const API_BASE_URL = 'https://talkvoid.rf.gd/api';
+const API_BASE_URL = 'https://talkvoid-backend-main-eaw2cx.free.laravel.cloud/api';
 
 function App() {
   const [isDark, setIsDark] = useState(() => {
